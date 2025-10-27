74b3fef2ce9994b7d325e7c35aeb3522ad70407e
Jonatan Lisama <jtlisama21@gmail.com>
2025-10-27 13:56:58 -0600

    Updates

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const toggle = document.querySelector('.nav__toggle');
 const menu = document.querySelector('.nav__list');
 
 toggle.addEventListener('click', () => {
-    console.log("click")
   menu.classList.toggle('nav__list--active');
   toggle.classList.toggle('nav__toggle--open');
 });
 
+const myNavlinks = menu.querySelectorAll('li');
+// console.log(mylink)
+
+myNavlinks.forEach(link => {
+  link.addEventListener('click', () => {
+    console.log('escuchandoo')
+    menu.classList.remove('nav__list--active')
+  })
+})
